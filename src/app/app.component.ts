03dae99eba8092b48e1a8936901ccf560df101f6
Juliusz Ceasar <marcin@o2.pl>
2023-10-22 14:30:05 +0200

    combobox, ngModelChange, filtrowanie listy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,27 @@ import { WishItem } from "../shared/models/wishItem";
 export class AppComponent {
   title = 'angularWebstormmmm';
   items : WishItem[] = [
-     new WishItem('To Learn Angular')
-    // new WishItem('Get Yerba', true),
-    // new WishItem('Sialalala', false)
+     new WishItem('To Learn Angular'),
+     new WishItem('Get Yerba', true),
+     new WishItem('Sialalala', false)
   ];
+  newWishText = '';
+  listFilter = '';
+  visibleItems = this.items;
 
   toggleItem(item: WishItem) {
     item.isComplete = !item.isComplete;
     console.log(item);
+  }
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+  }
+  filterChanged($event: any) {
+    switch ($event){
+      case "0": this.visibleItems = this.items; break;
+      case "1": this.visibleItems = this.items.filter(item => !item.isComplete); break;
+      case "2": this.visibleItems = this.items.filter(item => item.isComplete); break;
+    }
   }
 }
