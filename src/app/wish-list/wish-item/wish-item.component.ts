a96d41873a00b58eafa8e825d0726e686e31f077
Juliusz Ceasar <marcin@o2.pl>
2023-10-23 13:57:36 +0200

    zaimpementowanie wish-item

--- src/app/wish-list/wish-item/wish-item.component.ts
+++ src/app/wish-list/wish-item/wish-item.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
-  selector: 'app-wish-item',
+  selector: 'wish-item',
   templateUrl: './wish-item.component.html',
   styleUrls: ['./wish-item.component.css']
 })
 export class WishItemComponent implements OnInit {
+  @Input() wishText? : string;
+  @Output() wishTextChange = new EventEmitter();
+  @Input() wishComplete? : boolean
+  @Output() wishCompleteChange = new EventEmitter();
 
   constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  toggleComplete() {
+    this.wishComplete = !this.wishComplete;
+    this.wishCompleteChange.emit(this.wishComplete);
   }
-
 }
