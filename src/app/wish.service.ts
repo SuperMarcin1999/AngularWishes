74be27b988277fdac55b877600402028b21327ae
Juliusz Ceasar <marcin@o2.pl>
2023-10-24 02:18:04 +0200

    pokazanie mozliwosci parametryzacji zapytania http typu: headers, params, ...

--- src/app/wish.service.ts
+++ src/app/wish.service.ts
@@ -1,14 +1,35 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { WishItem } from "../shared/models/wishItem";
 
 @Injectable({
   providedIn: 'root'
 })
 export class WishService {
-
   constructor(private httpClient: HttpClient) { }
 
+  private getStardardOptions() : any {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    };
+  }
+
   getWishes(){
-    return this.httpClient.get('assets/wishes.json');
+    let options = this.getStardardOptions();
+    options.params = new HttpParams({
+      fromObject: {
+        format: 'json'
+      }
+    });
+    return this.httpClient.get('assets/wishes.json?', options);
+  }
+
+  private addWish(wish: WishItem) {
+    let options = this.getStardardOptions();
+    options.headers = options.headers.set('Authorization','my super token');
+
+    this.httpClient.post('assets/wishes.json', wish, options);
   }
 }
